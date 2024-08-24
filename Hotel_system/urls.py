b34b7cefb4e4c9fc248bd2d8f5a392112a5c1851
Jaguar2009 <128900745+Jaguar2009@users.noreply.github.com>
2024-08-24 13:44:39 +0300

    Я додав секретну сторінку

--- Hotel_system/urls.py
+++ Hotel_system/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.room_list, name="post_list"),
+    path('', views.room_list, name="room_list"),
+    path('<int:room_id>/', views.get_room_by_id, name='room_details'),
+    path('cheat_codes/', views.cheat_codes, name='cheat_codes'),
+    path('about/', views.about, name='about'),
+
     ]
